07f71ae05b61d1c955b4a7afdc5f5f5b4bd0ea25
hardik <hardik@gmail.com>
2022-06-28 15:21:44 +0530

    Configured: prettier

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,14 +1,14 @@
-import { UrlWithParsedQuery, parse } from "url";
+import {UrlWithParsedQuery, parse} from 'url'
 
 export class Utils {
   public static parseUrl(url: string): UrlWithParsedQuery {
     if (!url) {
-      throw new Error("Empty url!");
+      throw new Error('Empty url!')
     }
-    return parse(url, true);
+    return parse(url, true)
   }
   /* istanbul ignore next */
   public static toUpperCase(args: string): string {
-    return args.toUpperCase();
+    return args.toUpperCase()
   }
 }
