3939adafadbf54c0e2644f510870a8304f43b4dc
hardik <hardik@gmail.com>
2022-07-07 11:21:17 +0530

    create signup component & test it

--- src/App.js
+++ src/App.js
@@ -1,82 +1,15 @@
 import "./App.css";
-import { useState } from "react";
-import validator from "validator";
+import SignUp from "./Signup/Signup";
 
-function App() {
-  const [signupInput, setSignupInput] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-  const [error, setError] = useState("");
-  const handleChange = (e) => {
-    setSignupInput({
-      ...signupInput,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    if (!validator.isEmail(signupInput.email)) {
-      return setError("The email you input is invalid.");
-    } else if (signupInput.password.length < 5) {
-      return setError(
-        "The password you entered should contain 5 or more characters."
-      );
-    } else if (signupInput.password !== signupInput.confirmPassword) {
-      return setError("the password don't match. try again");
-    }
-  };
+const App = () => {
   return (
-    <div className="container my-5">
-      <form>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email Label
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="form-control"
-            value={signupInput.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="form-control"
-            value={signupInput.password}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="confirm-password" className="form-label">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            id="confirm-password"
-            name="confirmPassword"
-            className="form-control"
-            value={signupInput.confirmPassword}
-            onChange={handleChange}
-          />
-        </div>
-        {error && <p className="text-danger">{error}</p>}
-        <button type="submit" className="btn btn-primary" onClick={handleClick}>
-          Submit
-        </button>
-      </form>
+    <div>
+      <h1 data-testid="header" className="text-center text-muted m-5">
+        Wel-Come
+      </h1>
+      <SignUp />
     </div>
   );
-}
+};
 
 export default App;
