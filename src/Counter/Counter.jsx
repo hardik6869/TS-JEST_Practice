026ed4d8be9033e66b4f99d7f1b76a867952bc45
hardik <hardik@gmail.com>
2022-07-08 10:53:17 +0530

    Testing functionality with fireEvent

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  const [counter, setCounter] = useState("0");
+  const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState(1);
+
+  const addToCounter = () => {
+    setCounter(counter + inputValue);
+  };
+
+  const subtractToCounter = () => {
+    setCounter(counter - inputValue);
+  };
   return (
     <div className="container">
       <div className="text-center">
         <h3 data-testid="title">My Counter</h3>
-        <h2 data-testid="counter" className="py-3">
+        <h2
+          data-testid="counter"
+          className={`${counter >= 100 ? "text-success" : ""}${
+            counter <= -100 ? "text-danger" : ""
+          }`}
+        >
           {counter}
         </h2>
         <div className="text-center d-flex justify-content-center align-item-center py-3">
-          <button data-testid="addBtn" className="btn btn-success btn-lg">
+          <button
+            data-testid="addBtn"
+            className="btn btn-success btn-lg"
+            onClick={addToCounter}
+          >
             +
           </button>
           <input
@@ -19,8 +36,15 @@ const Counter = () => {
             type="number"
             className="mx-4 border-secondry text-center"
             value={inputValue}
+            onChange={(e) => {
+              setInputValue(parseInt(e.target.value));
+            }}
           />
-          <button data-testid="subtractBtn" className="btn btn-danger btn-lg">
+          <button
+            data-testid="subtractBtn"
+            className="btn btn-danger btn-lg"
+            onClick={subtractToCounter}
+          >
             -
           </button>
         </div>
--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, getByTestId, render, screen } from "@testing-library/react";
 import Counter from "../Counter/Counter";
 
 test("Header render with currect test", () => {
@@ -29,4 +29,119 @@ test("Subtract button renders with -", () => {
   render(<Counter />);
   const subtractBtn = screen.getByTestId("subtractBtn");
   expect(subtractBtn.textContent).toBe("-");
+});
+
+test("Change value of input works currectly", () => {
+  render(<Counter />);
+  const inputElement = screen.getByTestId("input");
+  expect(inputElement.value).toBe("1");
+  fireEvent.change(inputElement, {
+    target: { value: "5" },
+  });
+  expect(inputElement.value).toBe("5");
+});
+
+test("click on plus button adds 1 to counter", () => {
+  render(<Counter />);
+  const addBtnElement = screen.getByTestId("addBtn");
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement.textContent).toBe("0");
+  fireEvent.click(addBtnElement);
+  expect(counterElement.textContent).toBe("1");
+});
+
+test("click on subtract button subtracts 1 from counter", () => {
+  render(<Counter />);
+  const subtractBtnElement = screen.getByTestId("subtractBtn");
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement.textContent).toBe("0");
+  fireEvent.click(subtractBtnElement);
+  expect(counterElement.textContent).toBe("-1");
+});
+
+test("changing input value then clicking on add btn work currently", () => {
+  render(<Counter />);
+  const addBtnElement = screen.getByTestId("addBtn");
+  const counterElement = screen.getByTestId("counter");
+  const inputElement = screen.getByTestId("input");
+  fireEvent.change(inputElement, {
+    target: {
+      value: "5",
+    },
+  });
+  expect(counterElement.textContent).toBe("0");
+  fireEvent.click(addBtnElement);
+  expect(counterElement.textContent).toBe("5");
+});
+
+test("changing input value then clicking on subtract btn work currently", () => {
+  render(<Counter />);
+  const subtractBtnElement = screen.getByTestId("subtractBtn");
+  const counterElement = screen.getByTestId("counter");
+  const inputElement = screen.getByTestId("input");
+  fireEvent.change(inputElement, {
+    target: {
+      value: "5",
+    },
+  });
+  expect(counterElement.textContent).toBe("0");
+  fireEvent.click(subtractBtnElement);
+  expect(counterElement.textContent).toBe("-5");
+});
+
+test("adding and then subtracting leads to the correct counter number", () => {
+  render(<Counter />);
+  const addBtnElement = screen.getByTestId("addBtn");
+  const subtractBtnElement = screen.getByTestId("subtractBtn");
+  const counterElement = screen.getByTestId("counter");
+  const inputElement = screen.getByTestId("input");
+  fireEvent.change(inputElement, {
+    target: {
+      value: "10",
+    },
+  });
+  fireEvent.click(addBtnElement);
+  fireEvent.click(addBtnElement);
+  fireEvent.click(addBtnElement);
+  fireEvent.click(addBtnElement);
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  expect(counterElement.textContent).toBe("20");
+  fireEvent.change(inputElement, {
+    target: {
+      value: "5",
+    },
+  });
+  fireEvent.click(addBtnElement);
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  expect(counterElement.textContent).toBe("15");
+});
+
+test("counter contain correct class name", () => {
+  render(<Counter />);
+  const counterElement = screen.getByTestId("counter");
+  const inputElement = screen.getByTestId("input");
+  const addBtnElement = screen.getByTestId("addBtn");
+  const subtractBtnElement = screen.getByTestId("subtractBtn");
+  expect(counterElement.className).toBe("");
+  fireEvent.change(inputElement, {
+    target: {
+      value: "50",
+    },
+  });
+  fireEvent.click(addBtnElement);
+  expect(counterElement.className).toBe("");
+  fireEvent.click(addBtnElement);
+  expect(counterElement.className).toBe("text-success");
+  fireEvent.click(addBtnElement);
+  expect(counterElement.className).toBe("text-success");
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  expect(counterElement.className).toBe("");
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  expect(counterElement.className).toBe("text-danger");
 });
